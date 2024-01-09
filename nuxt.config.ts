62d2255f11fe81c6df90ea7d6d03ba75a6e855f3
mlavalle <marc-aurele.lavalle@epitech.eu>
2024-01-09 19:52:33 +0100

    Add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    'nuxt-icons', 
+    '@nuxtjs/tailwindcss', 
+    '@nuxt/image', 
+    '@vee-validate/nuxt', 
+    '@storyblok/nuxt'
+  ],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  },
+  storyblok: {
+    accessToken: process.env.STORYBLOK_ACCESS_TOKEN
+  },
+  components: {
+    dirs: [
+      {
+        path: '~/components/storyblok',
+        global: true,
+      }
+    ]
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    Hello World!
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
