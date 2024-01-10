f976c4680c88642db82f4f32003dcfdb56e2a3f2
mlavalle <marc-aurele.lavalle@epitech.eu>
2024-01-10 19:20:42 +0100

    dynamic pages, header, layout

---
+++ pages/[...slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<script setup>
+const { slug } = useRoute().params
+ 
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join('/') : 'home',
+  { version: 'draft' }
+)
+</script>
